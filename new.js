cf4ba2d404331b1cd3a16da59778b630f13d84d7
HussyDaddy <flybizit@gmail.com>
2023-07-30 23:28:30 +0530

    timesync solved

--- new.js
+++ new.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import Binance from 'binance-api-node';
+import axios from 'axios'; // Import axios library
 import config from './config.js';
-import currentBalance from './discord-webhook/current_balance.js'
-import futureOrderPlaced from './discord-webhook/future_placed.js'
+import currentBalance from './discord-webhook/current_balance.js';
+import futureOrderPlaced from './discord-webhook/future_placed.js';
 import errorBalanceWebhook from './discord-webhook/error_balance.js';
 import closingPosition from './discord-webhook/close_position.js';
 
-
-
-const port = 5002;
+const port = 3000;
 const app = express();
 
 app.use(bodyParser.json());
@@ -20,19 +19,29 @@ const client = Binance.default({
   httpFutures: config.httpFutures, // For prod
 });
 
+// Function to get the Binance server time
+async function getServerTime() {
+  try {
+    const response = await axios.get('https://api.binance.com/api/v3/time');
+    return response.data.serverTime;
+  } catch (error) {
+    console.log('Error getting server time:', error.message);
+    throw error;
+  }
+}
+
 app.get('/', async (req, res) => {
   console.log('root route working');
   res.send('root route working');
-})
+});
 
 app.post('/webhook', async (req, res) => {
   const alert = req.body;
   console.log('----RECEIVING order----');
   console.log('alert', alert);
   if (!alert.symbol) {
-    errorBalanceWebhook('Reciving Orders');
+    errorBalanceWebhook('Receiving Orders');
     return res.json({ message: 'ok' });
-    
   }
 
   try {
@@ -56,17 +65,23 @@ app.post('/webhook', async (req, res) => {
         const dSymbol = alert.symbol;
         const dSide = closePayload.side;
         const dQuantity = closePayload.quantity;
-        closingPosition(dSymbol,dSide,dQuantity);
+        closingPosition(dSymbol, dSide, dQuantity);
       }
     }
 
-    // Place new order
+    // Get Binance server time
+    const serverTime = await getServerTime();
+
+    // Calculate the correct timestamp for the order
+    const adjustedTimestamp = serverTime - 1000; // Adjusted by 1000 milliseconds (1 second)
+
+    // Place new order with the correct timestamp
     const payload = {
       symbol: alert.symbol,
       side: alert.side.toUpperCase(),
       type: 'MARKET',
       quantity: alert.quantity,
-      timestamp: alert.time,
+      timestamp: adjustedTimestamp,
       positionSide: alert.positionside.toUpperCase(),
     };
 
@@ -75,7 +90,7 @@ app.post('/webhook', async (req, res) => {
     const balance = await client.futuresAccountBalance();
     const usdtBalance = balance.find((b) => b.asset === 'USDT');
     console.log('USDT balance:', usdtBalance);
-    futureOrderPlaced(usdtBalance)
+    futureOrderPlaced(usdtBalance);
     return res.json({ message: 'ok' });
   } catch (e) {
     console.log('error', e);
@@ -85,7 +100,7 @@ app.post('/webhook', async (req, res) => {
 });
 
 app.listen(port, async () => {
-  console.log('Server listening on port' + port);
+  console.log('Server listening on port ' + port);
   try {
     const balance = await client.futuresAccountBalance();
     const usdtBalance = balance.find((b) => b.asset === 'USDT');
